6eb892a0dbce1a7089d948d52221e91ad96c22f5
doyoung <arktics@naver.com>
2018-04-03 23:44:44 +0900

    아이콘 추가, 네비게이션 드로어 수정

--- app/src/main/java/com/example/topping/topping/MainActivity.java
+++ app/src/main/java/com/example/topping/topping/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.topping.topping;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.text.InputType;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,7 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
@@ -87,21 +85,17 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Intent intent = null;
+        if (id == R.id.member_info) {
+            intent = new Intent(getApplicationContext(), MemberActivity.class);
+        } else if (id == R.id.like) {
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.search) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.write) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } /*else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }*/
-
+        }
+        startActivity(intent);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
