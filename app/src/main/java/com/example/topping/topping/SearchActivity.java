50f8d9a2a5e9739b07440e229cc6b6968bdf8e34
doyoung <arktics@naver.com>
2018-04-05 16:35:24 +0900

    작성글 페이지

--- app/src/main/java/com/example/topping/topping/SearchActivity.java
+++ app/src/main/java/com/example/topping/topping/SearchActivity.java
@@ -1,9 +1,14 @@
 package com.example.topping.topping;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private Button findBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,13 @@ public class SearchActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_keyboard_arrow_left_black_24dp);
+
+        findBtn = (Button)findViewById(R.id.search_find_btn);
+        findBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ContentActivity.class));
+            }
+        });
     }
 }
