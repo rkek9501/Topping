9c47f48fdb7051d62f733bb4dd589f9f2add9db5
doyoung <arktics@naver.com>
2018-04-15 20:57:42 +0900

    매칭 등록, 매칭 신청 버튼 다이얼로그

--- app/src/main/java/com/example/topping/topping/ContentActivity.java
+++ app/src/main/java/com/example/topping/topping/ContentActivity.java
@@ -1,12 +1,73 @@
 package com.example.topping.topping;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
 
-public class ContentActivity extends AbstractActivity {
+public class ContentActivity extends AbstractActivity implements View.OnClickListener {
+    private Button requestBtn, changeBtn;
+
+    private AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
+
+        requestBtn = (Button)findViewById(R.id.requestBtn);
+        changeBtn = (Button)findViewById(R.id.changeBtn);
+        requestBtn.setOnClickListener(this);
+        changeBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==requestBtn){
+            setRequsetDialog();
+        }else  if(v==changeBtn){
+            setChangeDialog();
+        }
+    }
+
+    private void setRequsetDialog() {
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle("확인");
+        builder.setMessage("매칭 신청 하시겠습니까?");
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                
+            }
+        });
+//        builder.setNegativeButton("아니오", null);
+        builder.setNeutralButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.create().show();
+    }
+    private void setChangeDialog() {
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle("확인");
+        builder.setMessage("내용을 수정하시겠습니까?");
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+//        builder.setNegativeButton("아니오", null);
+        builder.setNeutralButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.create().show();
     }
 }
--- app/src/main/java/com/example/topping/topping/FindActivity.java
+++ app/src/main/java/com/example/topping/topping/FindActivity.java
@@ -1,8 +1,15 @@
 package com.example.topping.topping;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
 
-public class FindActivity extends AbstractActivity {
+public class FindActivity extends AbstractActivity implements View.OnClickListener {
+    private Button submitBtn;
+
+    private AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,34 @@ public class FindActivity extends AbstractActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_keyboard_arrow_left_black_24dp);
 
+        submitBtn = (Button)findViewById(R.id.submitBtn);
+        submitBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==submitBtn){
+            setSubmitDialog();
+        }
+    }
+
+    private void setSubmitDialog() {
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle("확인");
+        builder.setMessage("매칭 신청 하시겠습니까?");
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+//        builder.setNegativeButton("아니오", null);
+        builder.setNeutralButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.create().show();
     }
 }
